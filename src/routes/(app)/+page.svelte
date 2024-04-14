6a87e136a9c94bc28bedc07c209006497df3efea
LeleDerGrasshalmi <king-of-gosts@web.de>
2024-04-14 20:15:42 +0200

    frontend - move uploader back to bottom

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -102,23 +102,50 @@
 			<a href={data.repository} target="_blank" class="font-bold"> Open Source</a>
 		</p>
 
-		<div class="mb-4 flex">
+		<div class="space-y-5 mb-4">
+			<div class="text-left">
+				<p>The idea is to automatically collect data from fortnite logs:</p>
+
+				<li>Profile Versions</li>
+				<li>API Errors</li>
+				<li>MMS Errors</li>
+				<li>to be continued ...</li>
+			</div>
+
+			<div class="text-left">
+				<p>We have tested the following log files:</p>
+
+				<li>EpicGamesLauncher</li>
+				<li>FortniteGame</li>
+				<li>UnrealEditorFortnite</li>
+				<li>WorldExplorers</li>
+			</div>
+
+			<div class="text-left">
+				<p>The following log files are known to be unsupported:</p>
+
+				<li>BuildPatchTool</li>
+				<li>ShooterGame</li>
+			</div>
+		</div>
+
+		{#if data.storageEnabled}
+			<div class="mt-2 mb-2 code">
+				<p class="font-bold text-xl">STORAGE DISCLAIMER</p>
+
+				<div class="mt-2 mb-2">
+					<p>We store submitted logs in our storage.</p>
+					<p>So we can analyze the logs later and get more data.</p>
+				</div>
+			</div>
+		{/if}
+
+		<div class="mb-4 flex gap-2">
 			<div>
 				<form>
 					<p class="font-bold text-2xl mb-2">Analyze your log</p>
 
 					<input bind:files type="file" accept=".log" bind:this={input} />
-
-					{#if data.storageEnabled}
-						<div class="mt-2 mb-2 code">
-							<p class="font-bold text-xl">STORAGE DISCLAIMER</p>
-
-							<div class="mt-2 mb-2">
-								<p>We store submitted logs in our storage.</p>
-								<p>So we can analyze the logs later and get more data.</p>
-							</div>
-						</div>
-					{/if}
 
 					<div class="mt-2 flex flex-row gap-2 justify-center">
 						<button class="mt-2 btn variant-filled" on:click={async () => await uploadLog()}>
@@ -153,33 +180,6 @@
 						<a href={data.clientRepository} target="_blank">Goto Client Repo</a>
 					</button>
 				</div>
-			</div>
-		</div>
-
-		<div class="space-y-5 mb-4">
-			<div class="text-left">
-				<p>The idea is to automatically collect data from fortnite logs:</p>
-
-				<li>Profile Versions</li>
-				<li>API Errors</li>
-				<li>MMS Errors</li>
-				<li>to be continued ...</li>
-			</div>
-
-			<div class="text-left">
-				<p>We have tested the following log files:</p>
-
-				<li>EpicGamesLauncher</li>
-				<li>FortniteGame</li>
-				<li>UnrealEditorFortnite</li>
-				<li>WorldExplorers</li>
-			</div>
-
-			<div class="text-left">
-				<p>The following log files are known to be unsupported:</p>
-
-				<li>BuildPatchTool</li>
-				<li>ShooterGame</li>
 			</div>
 		</div>
 	</div>
